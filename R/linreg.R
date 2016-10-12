7eb86e20d7fc52f22d539037a97ef1133d71e200
nickto <nickto0x2c@gmail.com>
2016-10-12 15:01:30 +0200

    Renamed package description file

--- R/linreg.R
+++ R/linreg.R
@@ -1,67 +1,16 @@
-#' Perform linear regression
+#' linreg: A for performing linear regressions and ridge expressions.
 #'
-#' Use \code{linreg} to perform linear regressions.
+#' The package provides six functions to manipulate linear regression
+#' models within a new class {\code{LinregClass}}: \itemize{
+#' \item{\code{\link{coef}} returns the coefficients as a named vector.}
+#' \item{\code{\link{resid}} returns the vector of residuals e.}
+#' \item{\code{pred} returns the predicted values.}
+#' \item{\code{\link{print}} prints the coefficients and coefficient names.}
+#' \item{\code{\link{plot}} creates two plots, residuals vs. fitted values and
+#' square root of residuals vs. fitted values.}
+#' \item{\code{\link{summary}} returns the coefficients with their standard
+#' error, t-value, p-value, estimated standard deviation and degrees of freedom.} }
 #'
-#' @param formula an object of \code{\link{class}} \code{\link{formula}} (or one
-#'   that can be coerced to that class): a symbolic description of the model to
-#'   be fitted.
-#' @param data a data frame or (or object coercible by
-#'   \code{\link{as.data.frame}}).
-#'
-#' @return \code{linreg} returns an object of class "\code{Linreg}".
-#'
-#'   An object of class "\code{Linreg}" have the following methods:
-#'   \item{coef()}{a named vector of coefficients}
-#'   \item{resid()}{the residuals, that is response minus fitted values}
-#'   \item{pred()}{predicted values of of a model}
-#'
-#' @seealso \code{\link{lm}}, \code{\link{class}}, \code{\link{formula}}
-#'
-#' @examples
-#' linreg(formula = Sepal.Length ~ Sepal.Width, data = iris)
-#'
-#' \dontrun{
-#' linreg(TRUE, TRUE)
-#' }
-#' @export
-
-linreg <- function(formula, data) {
-  suppressMessages( # without suppressing output of testthaat looks horrible
-    devtools::use_package("Matrix")
-  )
-  # Error handling -------------------------------------------------------------
-  # Check whether inputs can be coerced to formula and data.frame
-  canCoerse <- TRUE
-  tryCatch({
-    formula <- as.formula(formula)
-    data <- as.data.frame(data)
-  }, error = function(e) {
-    canCoerse <<- FALSE
-  })
-  if(!canCoerse) {
-    stop("wrong parameters")
-  }
-
-  # Check whether variables from formula are in data
-  if(!all(all.vars(formula) %in% colnames(data))) {
-    stop("variable(s) not in data")
-  }
-
-  # as.data.frame(data) should contain at leas 1 row/column
-  if(!all(dim(data) > 0)) {
-    stop("data must have >=1 cols/rows")
-  }
-
-  # check for multicollinearity of regressors
-  regressors <- model.matrix(formula, data)
-  regressorsRank <- Matrix::rankMatrix(regressors)
-  if (regressorsRank < ncol(regressors)) {
-    stop("multicollinearity of regressors")
-  }
-
-  # Body -----------------------------------------------------------------------
-  linregObject <- Linreg$new(formula = formula,
-                             data = data,
-                             call = match.call())
-  return(invisible(linregObject))
-}
+#' @docType package
+#' @name linreg
+NULL
