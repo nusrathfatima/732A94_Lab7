60add4f48e78f2b5372ad25122742b4573e71e98
unknown <joshu107@PC5-3.helix.ida.liu.se>
2016-10-12 20:25:15 +0200

    reverted to original state

--- R/linreg.R
+++ R/linreg.R
@@ -1,6 +1,4 @@
-#' Perform linear regression
 #'
-#' Use \code{linreg} to perform linear regressions.
 #'
 #' @param formula an object of \code{\link{class}} \code{\link{formula}} (or one
 #'   that can be coerced to that class): a symbolic description of the model to
@@ -8,14 +6,10 @@
 #' @param data a data frame or (or object coercible by
 #'   \code{\link{as.data.frame}}).
 #'
-#' @return \code{linreg} returns an object of class "\code{Linreg}".
 #'
-#'   An object of class "\code{Linreg}" have the following methods:
 #'   \item{coef()}{a named vector of coefficients}
-#'   \item{resid()}{the residuals, that is response minus fitted values}
 #'   \item{pred()}{predicted values of of a model}
 #'
-#' @seealso \code{\link{lm}}, \code{\link{class}}, \code{\link{formula}}
 #'
 #' @examples
 #' linreg(formula = Sepal.Length ~ Sepal.Width, data = iris)
