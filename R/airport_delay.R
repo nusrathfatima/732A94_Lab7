9fec197e8b108a544b08c4492b5c7448090d83e6
Sascha Schmalz <sassc662@student.liu.se>
2016-10-12 16:39:21 +0200

    The function now returns a ggplot object as specified in the task; Documentation was added;

--- R/airport_delay.R
+++ R/airport_delay.R
@@ -1,39 +1,55 @@
-library(dplyr)
-library(nycflights13)
-library(maps)
+#' Average departure and arrival delays
+#'
+#' The function calculates the average departure and arrival
+#' delays of of all airports of the dataset \code{nycflights13}.
+#' The function then prints a map of the United States of America and
+#' visualizes the average delay per airport in a point diagram.
+#' The size of the points represents the average delay time for the
+#' specific airport.
+#'
+#'
+#' @import dplyr
+#' @import maps
+#' @import nycflights13
+#'
+#' @return A map object
 
-# Prepare airport data for join
-airport <- nycflights13::airports %>%
-  mutate(ID = faa) %>%
-  select(-faa, -alt, -tz, -dst)
+airport_delay <- function() {
+  # Prepare airport data for join
+  airport <- nycflights13::airports %>%
+    mutate(ID = faa) %>%
+    select(-faa, -alt, -tz, -dst)
 
-# calculate mean values for each departure
-dep <- nycflights13::flights %>%
-  group_by(origin) %>%
-  summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
-  arrange(avg_dep_delay) %>%
-  mutate(ID = origin) %>%
-  select(-origin) %>%
-  right_join(airport, by = 'ID')
+  # calculate mean values for each departure
+  dep <- nycflights13::flights %>%
+    group_by(origin) %>%
+    summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
+    arrange(avg_dep_delay) %>%
+    mutate(ID = origin) %>%
+    select(-origin) %>%
+    right_join(airport, by = 'ID')
 
-# calculate mean values for each destination
-arr <- nycflights13::flights %>%
-  group_by(dest) %>%
-  summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
-  arrange(avg_arr_delay)%>%
-  mutate(ID = dest) %>%
-  select(-dest) %>%
-  right_join(airport, by = 'ID')
+  # calculate mean values for each destination
+  arr <- nycflights13::flights %>%
+    group_by(dest) %>%
+    summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+    arrange(avg_arr_delay)%>%
+    mutate(ID = dest) %>%
+    select(-dest) %>%
+    right_join(airport, by = 'ID')
+
+  res <- map(database = 'state') %>%
+    symbols(arr$lon, arr$lat,
+            bg = '#e2373f', fg = '#ffffff',
+            circles = sqrt(arr$avg_arr_delay),
+           inches = 0.1, add = TRUE, lwd = 0.5) %>%
+    symbols(dep$lon, dep$lat,
+            bg = '#233d4e', fg = '#ffffff',
+            circles = sqrt(dep$avg_dep_delay),
+            inches = 0.1, add = TRUE, lwd = 0.5) %>%
+    title('Average departure and arrival delays by US airports')
+
+  res
+}
 
 
-map(database = 'state')
-symbols(arr$lon, arr$lat,
-        bg = '#e2373f', fg = '#ffffff',
-        circles = sqrt(arr$avg_arr_delay),
-        inches = 0.1, add = TRUE, lwd = 0.5,
-        main = '')
-symbols(dep$lon, dep$lat,
-        bg = '#233d4e', fg = '#ffffff',
-        circles = sqrt(dep$avg_dep_delay),
-        inches = 0.1, add = TRUE, lwd = 0.5,
-        main = 'Average departute and arrival delay of US flights')
---
+++ R/airport_delay_ggplot.R
@@ -0,0 +1,86 @@
+#' Average departure and arrival delays
+#'
+#' The function calculates the average departure and arrival
+#' delays of of all airports of the dataset \code{nycflights13}.
+#' The function then prints a map of the United States of America and
+#' visualizes the average delay per airport in a point diagram.
+#' The size of the points represents the average delay time for the
+#' specific airport.
+#'
+#'
+#' @import dplyr
+#' @import maps
+#' @import nycflights13
+#'
+#' @return A ggplot2 object
+
+airport_delay <- function() {
+  # Prepare airport data for join
+  airport <- nycflights13::airports %>%
+    mutate(ID = faa) %>%
+    select(-faa, -alt, -tz, -dst)
+
+  # Calculate the table for all delayed departures
+  dep <- nycflights13::flights %>%
+    group_by(origin) %>%
+    summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
+    arrange(avg_dep_delay) %>%
+    mutate(ID = origin) %>%
+    select(-origin) %>%
+    right_join(airport, by = 'ID') %>%
+    filter(!is.na(avg_dep_delay)) %>%
+    mutate(type = ifelse(avg_dep_delay >= 0, 'behind', 'ahead')) %>%
+    mutate(avg_dep_delay = ifelse(avg_dep_delay < 0, avg_dep_delay * (-1), avg_dep_delay))
+
+  # Calculate the table for all delayed arrivals
+  arr <- nycflights13::flights %>%
+    group_by(dest) %>%
+    summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+    arrange(avg_arr_delay)%>%
+    mutate(ID = dest) %>%
+    select(-dest) %>%
+    right_join(airport, by = 'ID') %>%
+    filter(!is.na(avg_arr_delay)) %>%
+    mutate(type = ifelse(avg_arr_delay >= 0, 'behind', 'ahead')) %>%
+    mutate(avg_arr_delay = ifelse(avg_arr_delay < 0, avg_arr_delay * (-1), avg_arr_delay))
+
+  # Calculate the min and max longitude / latidute to determine the
+  # size of the maß
+  min_max_lon <- c(min(dep$lon, arr$lon), max(dep$lon, arr$lon)) * 1.02
+  min_max_lat <- c(min(dep$lat, arr$lat), max(dep$lat, arr$lat)) * 1.02
+
+  # Import a world map
+  us <- map_data('world')
+
+  # Make some specifications of the predefined '_minimal()' theme
+  # to make the plot look nicer
+  theme_map <- theme_minimal() %+replace% theme(
+    panel.background = element_rect(color = 'lightblue', fill = 'lightblue'),
+    panel.border = element_rect(color = 'NA', fill = 'NA'),
+    panel.grid = element_line(color = 'NA'),
+    panel.grid.major = element_line(color = 'NA'),
+    panel.grid.minor = element_line(color = 'NA'),
+    legend.position = 'none'
+  )
+
+  # Create the ggplot object and return it
+  ggplot() +
+    geom_polygon(data = us, fill = '#FFFFE0', color = 'black',
+                 aes(x = long, y = lat, group = group)) +
+    geom_point(data = arr, aes(x = arr$lon, y = arr$lat,
+                           fill = as.factor(type),
+                           size = sqrt(arr$avg_arr_delay),
+                           alpha = 0.7),
+               shape = 21) +
+    geom_point(data = dep, aes(x = dep$lon, y = dep$lat,
+                               fill = as.factor(type),
+                               size = sqrt(dep$avg_dep_delay),
+                               alpha = 0.7),
+               shape = 24) +
+    coord_cartesian(xlim = min_max_lon, ylim = min_max_lat) +
+    ggtitle('Average delay of flights at airports\nwithin the United States of America') +
+    xlab('Longitude [°]') +
+    ylab('Latitude [°]') +
+    theme_map
+}
+
