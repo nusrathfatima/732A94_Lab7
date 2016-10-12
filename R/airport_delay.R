b93aa05399c4af83af2b75073508ca88631b92ae
Sascha Schmalz <sassc662@student.liu.se>
2016-10-12 09:30:56 +0200

    final version of the airport delay

--- R/airport_delay.R
+++ R/airport_delay.R
@@ -1,5 +1,11 @@
+library(dplyr)
 library(nycflights13)
 library(maps)
+
+# Prepare airport data for join
+airport <- nycflights13::airports %>%
+  mutate(ID = faa) %>%
+  select(-faa, -alt, -tz, -dst)
 
 # calculate mean values for each departure
 dep <- nycflights13::flights %>%
@@ -7,7 +13,8 @@ dep <- nycflights13::flights %>%
   summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>%
   arrange(avg_dep_delay) %>%
   mutate(ID = origin) %>%
-  select(-origin)
+  select(-origin) %>%
+  right_join(airport, by = 'ID')
 
 # calculate mean values for each destination
 arr <- nycflights13::flights %>%
@@ -15,17 +22,18 @@ arr <- nycflights13::flights %>%
   summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
   arrange(avg_arr_delay)%>%
   mutate(ID = dest) %>%
-  select(-dest)
+  select(-dest) %>%
+  right_join(airport, by = 'ID')
 
-# Join all tables
-res <- nycflights13::airports %>%
-  mutate(ID = faa) %>%
-  select(-faa, -alt, -tz, -dst) %>%
-  left_join(dep, by = 'ID') %>%
-  left_join(arr, by = 'ID')
 
-map(database = 'world')
-symbols(res$lon, res$lat,
+map(database = 'state')
+symbols(arr$lon, arr$lat,
         bg = '#e2373f', fg = '#ffffff',
-        circles = sqrt(res$avg_arr_delay),
-        inches = 0.03, add = TRUE, lwd = 0.5)
+        circles = sqrt(arr$avg_arr_delay),
+        inches = 0.1, add = TRUE, lwd = 0.5,
+        main = '')
+symbols(dep$lon, dep$lat,
+        bg = '#233d4e', fg = '#ffffff',
+        circles = sqrt(dep$avg_dep_delay),
+        inches = 0.1, add = TRUE, lwd = 0.5,
+        main = '')
