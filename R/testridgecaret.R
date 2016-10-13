b60b09521878e17b2230a705d9771ef257722f47
nickto <nickto0x2c@gmail.com>
2016-10-13 22:31:16 +0200

    Commented the whole testridgecaret.R file
    Commented, because otherwise cannot build a package

--- R/testridgecaret.R
+++ R/testridgecaret.R
@@ -1,24 +1,24 @@
-
-  data("BostonHousing")
-  dat <- BostonHousing
-  trainIndex <- caret::createDataPartition(dat$age, p = .75,
-                                           list = FALSE,
-                                           times= 1)
-  datTrain <- dat[trainIndex, ]
-  datTest <- dat[-trainIndex, ]
-  res <- c(datTrain, datTest)
-  fitControl <- trainControl(method = "cv",
-                             number = 10)
-  # Set seq of lambda to test
-  lambdaGrid <- expand.grid(lambda = c(0,.01,.02,.03,.04))
-  ridge <- train(crim~.,
-                 data = datTrain,
-                 method='ridge',
-                 trControl = fitControl,
-                                tuneGrid = lambdaGrid,
-                 preProcess=c('center', 'scale')
-  )
-  predict(ridge$finalModel, type='coef', mode='norm')$coefficients[13,]
-  ridge.pred <- predict(ridge, datTest)
-  avgErrror<-2*sqrt(mean(ridge.pred - datTest$crim)^2)
-  ridge
+#
+#   data("BostonHousing")
+#   dat <- BostonHousing
+#   trainIndex <- caret::createDataPartition(dat$age, p = .75,
+#                                            list = FALSE,
+#                                            times= 1)
+#   datTrain <- dat[trainIndex, ]
+#   datTest <- dat[-trainIndex, ]
+#   res <- c(datTrain, datTest)
+#   fitControl <- trainControl(method = "cv",
+#                              number = 10)
+#   # Set seq of lambda to test
+#   lambdaGrid <- expand.grid(lambda = c(0,.01,.02,.03,.04))
+#   ridge <- train(crim~.,
+#                  data = datTrain,
+#                  method='ridge',
+#                  trControl = fitControl,
+#                                 tuneGrid = lambdaGrid,
+#                  preProcess=c('center', 'scale')
+#   )
+#   predict(ridge$finalModel, type='coef', mode='norm')$coefficients[13,]
+#   ridge.pred <- predict(ridge, datTest)
+#   avgErrror<-2*sqrt(mean(ridge.pred - datTest$crim)^2)
+#   ridge
